c21317e4035809e92182d402e1f79ccf36fff313
couto0 <daniel.couto64@gmail.com>
2022-05-29 16:16:37 -0300

    sistema de limpeza

--- lambda_function.py
+++ lambda_function.py
@@ -2,18 +2,23 @@ import requests
 import csv
 import json
 from io import StringIO
-from datetime import date
+from datetime import datetime, timedelta
 from telegram import Bot
 from mycredentials import TOKEN, SHEET_URL, CHAT_ID
 
+PLACES = ['Sala', 'Cozinha', 'Banheiro Social', 'Suite']
+
 class EEBot:
 
-    def __init__(self, cleaning_sheet_url: str, token: str, chat_id: str):
+    def __init__(self, cleaning_sheet_url: str, token: str, chat_id: str) -> None:
         self.cleaning_sheet_url = cleaning_sheet_url
         self.telegram_bot = Bot(token=token)
+        self.today = datetime.now() + timedelta(hours=-3) #GMT-3
         self.chat_id = chat_id
-        self.cleaning_sheet = ''
-        self.today_workers = ''
+        self.cleaning_sheet = []
+        self.today_place_workers = {}
+        self.today_trash_n_dishes = []
+        self.today_message = ''
 
 
     def get_cleaning_sheet(self) -> None:
@@ -22,24 +27,51 @@ class EEBot:
         csvfile = StringIO(response.content.decode())
         
         csvreader = csv.reader(csvfile)
-        res = ''
         for row in csvreader:
-            res = res + str(row) + '\n'
+            self.cleaning_sheet.append(row)
 
-        self.cleaning_sheet = res
+    def get_week_cycle(self) -> int:
+        alpha = datetime(year=2022, month=5, day=28)
+        omega = self.today
+        week_cycle = int((omega - alpha).days/7)%3
+        return week_cycle
 
-    def get_today_worker(self) -> None:
-        today = date.today().weekday()
-        print(today)
+    def get_today_place_workers(self) -> None:
+        week_cycle = self.get_week_cycle()
+        place_workers = self.cleaning_sheet[week_cycle+2][0:4]
+        for i in range(4):
+            self.today_place_workers[PLACES[i]] = place_workers[i]
+    
+    def get_today_trash_n_dishes(self) -> None:
+        weekday = self.today.weekday()
+        if weekday  in [0, 2, 4]:
+            self.today_trash_n_dishes = [self.cleaning_sheet[2][int(weekday /2)+5], 
+                                         self.cleaning_sheet[3][int(weekday /2)+5]]
 
-    def send_today_workers(self) -> None:
-        self.telegram_bot.sendMessage(chat_id=self.chat_id, text=self.cleaning_sheet)
+    def make_today_message(self) -> None:
+        message = "Escala de hoje ({}/{})\n\n".format(self.today.day, self.today.month)
+        if self.today_trash_n_dishes:
+            message += "Tirar lixo e guardar louça:\n{} e {}\n\n".format(*self.today_trash_n_dishes)
+        else:
+            message += "Ninguém tira o lixo e guarda louça hoje :(\n\n"
+        message += "Escala semanal ({}/{}):\n\n".format(self.today.day, self.today.month)
+        places = self.today_place_workers.keys()
+        for place in places:
+            message += "{}: {}\n".format(place, self.today_place_workers[place])
+        self.today_message=message
+
+
+    def send_today_message(self) -> None:
+        self.telegram_bot.sendMessage(chat_id=self.chat_id, text=self.today_message)
 
 
 def main() -> None:
     eebot = EEBot(SHEET_URL, TOKEN, CHAT_ID)
     eebot.get_cleaning_sheet()
-    eebot.send_today_workers()
+    eebot.get_today_place_workers()
+    eebot.get_today_trash_n_dishes()
+    eebot.make_today_message()
+    eebot.send_today_message()
 
 
 def lambda_handler(event, context):
@@ -49,3 +81,5 @@ def lambda_handler(event, context):
         'body': json.dumps('Sucess')
     }
 
+if __name__ == "__main__":
+    main()
